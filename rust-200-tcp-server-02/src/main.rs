8a687a2a8c9de8a60ddc81d79de5b9de465e6287
callor <callor@callor.com>
2024-11-02 04:09:06 +0900

    server

--- rust-200-tcp-server-02/src/main.rs
+++ rust-200-tcp-server-02/src/main.rs
@@ -36,7 +36,6 @@ fn handle_connection(mut stream: TcpStream) {
         let response = format!(
             "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
         );
-
         stream.write_all(response.as_bytes()).unwrap();
     }
 }
