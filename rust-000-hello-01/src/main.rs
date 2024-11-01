a51c800b43d6a2cd102acc8b7387cb119c1e2f20
callor <callor@callor.com>
2024-11-01 16:49:58 +0900

    상수선언

--- rust-000-hello-01/src/main.rs
+++ rust-000-hello-01/src/main.rs
@@ -1,24 +1,23 @@
 fn main() {
-
     /*
     변수 선언
     let 키워드를 사용하며 기본값으로 immutable 변수를 선언한다.
      */
     let name = "홍길동";
     println!("안녕하세요, {}!", name);
-  
+
     // cannot assign twice to immutable variable
     // immutable 변수는 값을 변경할 수 없다. 컴파일 오류발생
     // name = "이몽룡";
-    
-    // shadowing 
+
+    // shadowing
     // immutable 변수를 mutable 변수로 변경하는 방법(?)으로
     // 같은 이름의 변수를 선언하면서 let 키워드를 사용한다.
     // 이미 선언된 변수는 컴파일 과정에서 다른 이름으로 변경되고
     // 새롭게 선언된 변수는 이후의 코드에서 사용(read) 할 수 있다
     let name = "이몽룡";
     println!("안녕하세요, {}!", name);
-  
+
     let mut nation;
     /*
     변수에 값을 할당하면 최소 한번 이상 사용(read, use) 해야 한다
@@ -26,12 +25,11 @@ fn main() {
      */
     nation = "대한민국";
     println!("{} 만세", nation);
-  
+
     nation = "Korea";
     println!("{} 만세", nation);
-  
+
     println!("대한민국만세");
-  
 
     // type 지정하여 변수선언하기
     let num: i32 = 5;
@@ -43,6 +41,4 @@ fn main() {
 
     let str: String = String::from("Hello, world!");
     println!("문자열 변수 {str}");
-  
-  
-  }
+}
--- rust-000-hello-02/src/main.rs
+++ rust-000-hello-02/src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
     println!("Hello, world!");
+    /*
+    8비트 정수	i8
+    16비트 정수	i16
+    32비트 정수	i32
+    64비트 정수	i64
+    128비트 정수	i128
+    아키텍처	isize
+    부호 없는 8비트 정수	u8
+    부호 없는 16비트 정수	u16
+    부호 없는 32비트 정수	u32
+    부호 없는 64비트 정수	u64
+    부호 없는 128비트 정수	u128
+    부호 없는 아키텍처	usize
+    불리언	bool
+    문자열	String
+    문자열 슬라이스	str
+    32비트 부동소수점 실수	f32
+    64비트 부동소수점 실수	f64
+     */
+
+
+     // 변수 형 변환
+     let num1: f64 = 1.2;
+     let num2 = num1 as i32;
+     println!("type Cast : f64({num1}) -> i32({num2})");
+
+
+     // 상수선언
+     const MAX_POINTS: u32 = 100_000;
+     println!("상수선언 : MAX_POINTS = {MAX_POINTS}");
+
 }
