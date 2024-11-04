62f5b444b5e269b07cb3ffc6f5bab7cd4870ff63
callor <callor@callor.com>
2024-11-04 12:11:17 +0900

    slice

--- rust-000-hello-04/src/main.rs
+++ rust-000-hello-04/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello Korea");
+
+    println!("s: {}", first_word(&s));
+    let len = s.len();
+
+    let slice = &s[3..len];
+    println!("slice: {}", slice);
+
+
+    let slice = &s[3..];
+    println!("slice: {}", slice);
+
 }
+
+
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
