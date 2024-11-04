64c48d2ca59d20afe355d1b1e912f8c47ceecb96
callor <callor@callor.com>
2024-11-04 13:35:26 +0900

    module

---
+++ rust-010-module-01/src/libj.rs
@@ -0,0 +1,17 @@
+pub mod front_of_house {
+  pub mod hosting {
+      fn add_to_waitlist() {
+        println!("add_to_waitlist");
+      }
+
+      fn seat_at_table() {}
+  }
+
+  mod serving {
+      fn take_order() {}
+
+      fn serve_order() {}
+
+      fn take_payment() {}
+  }
+}
