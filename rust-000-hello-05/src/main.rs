2a012541fd584941e9f4e57851ae3e87076f0d6b
callor <callor@callor.com>
2024-11-04 09:34:58 +0900

    tup

--- rust-000-hello-05/src/main.rs
+++ rust-000-hello-05/src/main.rs
@@ -1,3 +1,15 @@
+
 fn main() {
-    println!("Hello, world!");
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+
+    println!("The value of five_hundred is: {}", five_hundred);
+    println!("The value of six_point_four is: {}", six_point_four);
+    println!("The value of one is: {}", one);
+    println!("The value of x is: {}",x.1);
+
+    let tup:() = ();
+    println!("The value of tup is: {:?}", tup);
 }
